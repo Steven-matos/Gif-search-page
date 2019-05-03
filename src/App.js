896b3662aa91c06f72d0614b37f85127806b5e7c
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-05-03 17:18:03 -0400

    updated page and created build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
-  } 
+    this.state = {
+      gifs: [],
+      loading: true
+    };
+  }
 
-  render() { 
+    componentDidMount(){
+      this.performSearch();
+    }
+
+    performSearch = (query = 'Shiba') => {
+      axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=A1f0ssS44F7XZuxQyGQww1FEkGvqGVlD`)
+        .then(response => {
+            this.setState({
+              gifs: response.data.data,
+              loading: false
+            });
+        })
+        .catch(error => {
+          console.log('Error fetching and parsing data', error);
+        });
+
+    }
+
+  render() {
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm onSearch={this.performSearch} />
+          </div>
+        </div>
         <div className="main-content">
-          <GifList />
+          {
+            (this.state.loading)
+              ? <p className="loading">Loading...</p>
+              : <GifList data={this.state.gifs}/>
+          }
         </div>
       </div>
     );
